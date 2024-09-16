1336a1c5b81370d75da6d60f0c2cc3d5f9104f72
Andherson Castro <agcl82@gmail.com>
2024-09-15 23:02:36 -0400

    Testing

---
+++ functions/api.js
@@ -0,0 +1,21 @@
+import express, { application } from "express";
+import ServerlessHttp from "serverless-http";
+
+
+const app = express();
+
+//Routes
+
+app.get(",", (req, res)=>{
+    return res.json({
+        messages:"hello world"
+    })
+})
+
+
+const handler = ServerlessHttp(app)
+
+module.exports.handler = async(event, context) => {
+    const result = await handler(event, context);
+    return result;
+}
