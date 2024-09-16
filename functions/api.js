0ee4e571b07dd10f036689ea5cf5fafb421727b9
Andherson Castro <agcl82@gmail.com>
2024-09-15 23:18:42 -0400

    test6

--- functions/api.js
+++ functions/api.js
@@ -4,7 +4,7 @@ import ServerlessHttp from "serverless-http";
 const app = express();
 
 
-app.get('/.orders', (req, res) => {
+app.get('/.netlify/functions/api', (req, res) => {
     return res.json({
         messages: "hello world!"
     })
